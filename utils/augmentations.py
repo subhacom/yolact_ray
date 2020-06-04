7403b9ae8104888b8cc4ed5cdcefa6011c2abd0d
Subhasis Ray <ray.subhasis@gmail.com>
2020-06-04 16:59:22 -0400

    Fixed cuda dependence.

--- utils/augmentations.py
+++ utils/augmentations.py
@@ -622,9 +622,12 @@ class FastBaseTransform(torch.nn.Module):
 
     def __init__(self):
         super().__init__()
-
-        self.mean = torch.Tensor(MEANS).float().cuda()[None, :, None, None]
-        self.std  = torch.Tensor( STD ).float().cuda()[None, :, None, None]
+        if torch.cuda.is_available():
+            self.mean = torch.Tensor(MEANS).float().cuda()[None, :, None, None]
+            self.std  = torch.Tensor( STD ).float().cuda()[None, :, None, None]
+        else:
+            self.mean = torch.Tensor(MEANS).float()[None, :, None, None]
+            self.std = torch.Tensor(STD).float()[None, :, None, None]
         self.transform = cfg.backbone.transform
 
     def forward(self, img):
